864afeee256ac98eb087c31e1128161d16d8b834
miyuru-vs1bv <miyuru@bevolv.co>
2025-05-02 15:30:16 +0530

    fix console log

--- packages/search/src/features/records/service.ts
+++ packages/search/src/features/records/service.ts
@@ -1049,12 +1049,15 @@ export async function getRecordById<T extends Array<keyof StateIdenfitiers>>(
 ): Promise<StateIdenfitiers[T[number]]> {
   const db = client.db()
   const query = aggregateRecords({ recordId, includeHistoryResources })
+
+  console.log("=====================\n",JSON.stringify(query),"\n=====================")
+
   const result = await db
     .collection('Composition')
     .aggregate<Bundle>(query, { allowDiskUse: true })
     .toArray()
 
-  console.log(JSON.stringify(query))
+  console.log(JSON.stringify(result))
 
   const bundle = result[0]
 
