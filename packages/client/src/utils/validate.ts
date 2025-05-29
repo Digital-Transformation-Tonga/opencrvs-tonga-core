5a24266cab4f4c2bafa684da291b4eaa6250815d
anjana6 <anjanashakthi95@gmail.com>
2025-05-29 16:14:56 +0530

    Reapply "Tonga/crvs update"
    This reverts commit 875bdb062389efcf9542942d4bc08819a7e71e60, reversing
    changes made to 60b38b1cf86b9c1fbcdf57efcbed233241392e18.

--- packages/client/src/utils/validate.ts
+++ packages/client/src/utils/validate.ts
@@ -239,10 +239,6 @@ export const isDateNotInFuture = (date: string) => {
   return new Date(date) <= new Date(Date.now())
 }
 
-export const isDateNotPastLimit = (date: string, limit: Date) => {
-  return new Date(date) >= limit
-}
-
 export const isDateNotBeforeBirth = (date: string, drafts: IFormData) => {
   const birthDate = drafts?.deceased?.birthDate as string
   return birthDate ? new Date(date) >= new Date(birthDate) : true
@@ -307,30 +303,29 @@ export const isValidBirthDate: Validation = (
   return !cast
     ? { message: messages.required }
     : cast &&
-      isDateNotInFuture(cast) &&
-      isAValidDateFormat(cast) &&
-      isDateNotAfterBirthEvent(cast, drafts as IFormData)
-    ? isDateNotAfterDeath(cast, drafts as IFormData)
-      ? undefined
+        isDateNotInFuture(cast) &&
+        isAValidDateFormat(cast) &&
+        isDateNotAfterBirthEvent(cast, drafts as IFormData)
+      ? isDateNotAfterDeath(cast, drafts as IFormData)
+        ? undefined
+        : {
+            message: messages.isDateNotAfterDeath
+          }
       : {
-          message: messages.isDateNotAfterDeath
+          message: messages.isValidBirthDate
         }
-    : {
-        message: messages.isValidBirthDate
-      }
 }
 
 export const isValidChildBirthDate: Validation = (value: IFormFieldValue) => {
   const childBirthDate = value as string
-  const pastDateLimit = new Date(1800, 0, 1)
+
   return !childBirthDate
     ? { message: messages.required }
     : childBirthDate &&
-      isAValidDateFormat(childBirthDate) &&
-      isDateNotInFuture(childBirthDate) &&
-      isDateNotPastLimit(childBirthDate, pastDateLimit)
-    ? undefined
-    : { message: messages.isValidBirthDate }
+        isAValidDateFormat(childBirthDate) &&
+        isDateNotInFuture(childBirthDate)
+      ? undefined
+      : { message: messages.isValidBirthDate }
 }
 
 export const isValidParentsBirthDate =
@@ -518,17 +513,6 @@ export const dateNotInFuture = (): Validation => (value: IFormFieldValue) => {
     return { message: messages.dateFormat }
   }
 }
-
-export const dateNotPastLimit =
-  (limit: string): Validation =>
-  (value: IFormFieldValue) => {
-    const cast = value as string
-    if (isDateNotPastLimit(cast, new Date(limit))) {
-      return undefined
-    } else {
-      return { message: messages.dateFormat }
-    }
-  }
 
 export const dateNotToday = (date: string): boolean => {
   const today = new Date().setHours(0, 0, 0, 0)
@@ -777,8 +761,8 @@ export const greaterThanZero: Validation = (value: IFormFieldValue) => {
   return !value && value !== 0
     ? { message: messages.required }
     : value && Number(value) > 0
-    ? undefined
-    : { message: messages.greaterThanZero }
+      ? undefined
+      : { message: messages.greaterThanZero }
 }
 
 export const notGreaterThan =
