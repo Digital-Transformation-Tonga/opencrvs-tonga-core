75b651373c08225397c3aad17a7a28abf7956f72
anjana6 <anjanashakthi@gmail.com>
2026-07-23 16:41:30 +0530

    bug: fixed issue 0179

--- packages/client/src/v2-events/components/forms/inputs/FileInput/DocumentUploaderWithOption.tsx
+++ packages/client/src/v2-events/components/forms/inputs/FileInput/DocumentUploaderWithOption.tsx
@@ -30,7 +30,6 @@ import { useImageProcessing } from '@client/utils/imageUtils'
 import { DocumentUploader } from './SimpleDocumentUploader'
 import { DocumentListPreview } from './DocumentListPreview'
 import { DocumentPreview } from './DocumentPreview'
-import { File } from './FileInput'
 import { useOnFileChange } from './useOnFileChange'
 import { SingleDocumentPreview } from './SingleDocumentPreview'
 
@@ -49,8 +48,13 @@ const Flex = styled.div`
 
 function getUpdatedFiles(
   prevFiles: FileFieldValueWithOption[],
-  newFile: FileFieldValueWithOption
+  newFile: FileFieldValueWithOption,
+  allowMultiplePerOption = false
 ) {
+  if (allowMultiplePerOption) {
+    return [...prevFiles, newFile]
+  }
+
   return [
     ...prevFiles.filter((prevFile) => prevFile.option !== newFile.option),
     newFile
@@ -117,6 +121,8 @@ function DocumentUploaderWithOption({
     useState<FileFieldValueWithOption | null>(null)
   const { processImageFile } = useImageProcessing()
 
+  const allowMultiplePerOption = options.length === 1
+
   const { uploadFile } = useFileUpload(name, {
     onSuccess: ({ type, originalFilename, path, id }) => {
       const newFile = {
@@ -128,9 +134,19 @@ function DocumentUploaderWithOption({
 
       setFilesBeingProcessed((prev) => prev.filter(({ label }) => label !== id))
 
-      setFiles((prevFiles) => getUpdatedFiles(prevFiles, newFile))
-      onChange(getUpdatedFiles(files, newFile))
-      setSelectedOption(undefined)
+      setFiles((prevFiles) => {
+        const updatedFiles = getUpdatedFiles(
+          prevFiles,
+          newFile,
+          allowMultiplePerOption
+        )
+        onChange(updatedFiles)
+        return updatedFiles
+      })
+
+      if (!allowMultiplePerOption) {
+        setSelectedOption(undefined)
+      }
     }
   })
 
@@ -181,49 +197,26 @@ function DocumentUploaderWithOption({
     setPreviewImage(null)
   }
 
-  const remainingOptions = options.filter(
-    ({ value: val }) => !files.some((file) => file.option === val)
-  )
+  // Keep the only option available so users can upload multiple "Other" files.
+  const remainingOptions = allowMultiplePerOption
+    ? options
+    : options.filter(
+        ({ value: val }) => !files.some((file) => file.option === val)
+      )
 
   if (hideOnEmptyOption && remainingOptions.length === 0) {
     return null
   }
 
-  if (options.length === 1) {
-    const [onlyOption] = options
-    return (
-      <File.Input
-        acceptedFileTypes={acceptedFileTypes}
-        description={description}
-        disabled={disabled}
-        error={error}
-        label={
-          typeof onlyOption.label === 'string'
-            ? onlyOption.label
-            : intl.formatMessage(onlyOption.label)
-        }
-        maxFileSize={maxFileSize}
-        name={name}
-        value={value[0]}
-        width={'full'}
-        onChange={(file) => {
-          if (file) {
-            onChange([{ ...file, option: onlyOption.value }])
-          }
-        }}
-      />
-    )
-  }
-
   if (
-    autoSelectOnlyOption &&
+    (allowMultiplePerOption || autoSelectOnlyOption) &&
     remainingOptions.length === 1 &&
     remainingOptions[0].value !== selectedOption
   ) {
     setSelectedOption(remainingOptions[0].value)
   }
 
-  const errorMessage = unselectedOptionError || fileChangeError || ''
+  const errorMessage = unselectedOptionError || fileChangeError || error || ''
 
   return (
     <UploadWrapper>
@@ -243,22 +236,9 @@ function DocumentUploaderWithOption({
         }
       />
 
-      <Flex>
-        <DropdownContainer>
-          <Select.Input
-            disabled={disabled}
-            id={name}
-            options={remainingOptions}
-            type={'SELECT'}
-            value={selectedOption}
-            onChange={(val) => {
-              void helpers.setTouched(true)
-              setSelectedOption(val)
-              setUnselectedOptionError('')
-            }}
-          />
-        </DropdownContainer>
+      {allowMultiplePerOption ? (
         <DocumentUploader
+          fullWidth
           disabled={!selectedOption || disabled}
           id={name}
           name={name}
@@ -266,7 +246,32 @@ function DocumentUploaderWithOption({
         >
           {intl.formatMessage(messages.uploadFile)}
         </DocumentUploader>
-      </Flex>
+      ) : (
+        <Flex>
+          <DropdownContainer>
+            <Select.Input
+              disabled={disabled}
+              id={name}
+              options={remainingOptions}
+              type={'SELECT'}
+              value={selectedOption}
+              onChange={(val) => {
+                void helpers.setTouched(true)
+                setSelectedOption(val)
+                setUnselectedOptionError('')
+              }}
+            />
+          </DropdownContainer>
+          <DocumentUploader
+            disabled={!selectedOption || disabled}
+            id={name}
+            name={name}
+            onChange={handleFileChange}
+          >
+            {intl.formatMessage(messages.uploadFile)}
+          </DocumentUploader>
+        </Flex>
+      )}
 
       {previewImage && (
         <DocumentPreview
